0d12e84252adb0bca1293d59f77f4b6dd70e7379
Thomas Hettasch <t.hettasch@tudelft.nl>
2026-07-27 12:55:21 +0000

    Reorder FT data so the frames are correct

--- src/egm_common_auxiliary.cpp
+++ src/egm_common_auxiliary.cpp
@@ -854,12 +854,12 @@ bool parse(wrapper::ForceTorque* p_target, const EgmMeasuredForce& source)
   if (source.force_size() == 6)
   {
     // Read out force torque data (6 DOF)
-    p_target->mutable_force()->set_x(source.force().at(0));
-    p_target->mutable_force()->set_y(source.force().at(1));
-    p_target->mutable_force()->set_z(source.force().at(2));
-    p_target->mutable_torque()->set_x(source.force().at(3));
-    p_target->mutable_torque()->set_y(source.force().at(4));
-    p_target->mutable_torque()->set_z(source.force().at(5));
+    p_target->mutable_force()->set_x(source.force().at(2));
+    p_target->mutable_force()->set_y(-source.force().at(1));
+    p_target->mutable_force()->set_z(-source.force().at(0));
+    p_target->mutable_torque()->set_x(source.force().at(5));
+    p_target->mutable_torque()->set_y(-source.force().at(4));
+    p_target->mutable_torque()->set_z(-source.force().at(3));
     return true;
   }
   else if (source.force_size() == 1)
